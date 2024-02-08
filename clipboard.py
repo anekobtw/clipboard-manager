ee66c5d81e255d6ab7bd5eacc8c132a6c423b1ab
ssayand <krolexonder@gmail.com>
2024-02-08 18:50:06 +0400

    GUI and bug fixes
    - sorted imports
    - added GUI
    - bug fixes
    - improved code readability

--- clipboard.py
+++ clipboard.py
@@ -1,28 +1,76 @@
-import win32clipboard
-from colorama import Fore, init
-import keyboard
-import os
+import threading
 import time
+from tkinter import Tk
 
-os.system('cls' if os.name == 'nt' else 'clear')
-print(f'{Fore.YELLOW}Clipboard history is empty yet.')
-init(autoreset=True)
-cb = []
+import customtkinter
+import keyboard
+import win32clipboard
 
 
-def get_data():
-    time.sleep(0.1)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.is_shown = True
+
+    def create_widgets(self):
+        self.option_menu = customtkinter.CTkOptionMenu(self, values=cb)
+        self.option_menu.pack(padx=5, pady=5)
+
+        self.button = customtkinter.CTkButton(self, text='Insert', command=self.insert_text)
+        self.button.pack(padx=5, pady=5)
+
+    def update_option_menu(self):
+        self.option_menu.configure(values=cb)
+
+    def insert_text(self):
+        text = self.option_menu.get()
+        self.is_shown = False
+        self.withdraw()
+        keyboard.write(text)
+
+
+def get_data(app: App):
+    time.sleep(0.1)  # prevents a bug
+
     win32clipboard.OpenClipboard()
     data = win32clipboard.GetClipboardData()
-    if data not in cb:
-        cb.append(data)
     win32clipboard.CloseClipboard()
 
-    os.system('cls' if os.name == 'nt' else 'clear')
-    for i in range(len(cb)):
-        print(f"{Fore.GREEN}{i}. {Fore.LIGHTBLUE_EX}{cb[i]}")
+    if data not in cb:
+        cb.append(data)
+        app.update_option_menu()
 
 
-while True:
-    keyboard.add_hotkey('ctrl + c', get_data)
-    keyboard.wait()
+def keyboard_listener1(app: App):
+    while True:
+        keyboard.wait('ctrl + c')
+        get_data(app)
+
+
+def keyboard_listener2(app: App):
+    while True:
+        keyboard.wait('win + v')
+        if app.is_shown:
+            app.withdraw()
+            app.is_shown = False
+        else:
+            x, y = Tk().winfo_pointerxy()
+            app.geometry(f'+{x}+{y}')
+            app.deiconify()
+            app.is_shown = True
+
+
+if __name__ == '__main__':
+    cb = []
+
+    app = App()
+    app.create_widgets()
+
+    keyboard_thread1 = threading.Thread(target=keyboard_listener1, args=(app,), daemon=True)
+    keyboard_thread1.start()
+
+    keyboard_thread2 = threading.Thread(target=keyboard_listener2, args=(app,), daemon=True)
+    keyboard_thread2.start()
+
+    app.mainloop()
+
