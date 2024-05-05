4aed3cecbd15fa92e00adeeb6413e471ec6090a7
ssayand <krolexonder@gmail.com>
2024-05-05 13:19:08 +0400

    significant changes and bug fix

--- README.md
+++ README.md
@@ -1,10 +1,41 @@
 # Clipboard Manager
-
 The Clipboard Manager is a Python application designed to enhance clipboard management on your system. It provides a user-friendly interface along with convenient keyboard shortcuts for efficient handling of clipboard content.
 
-## Demo
-https://github.com/anekobtw/clipboard-manager/blob/main/preview.mp4
+## Features
+- Clipboard Monitoring: The application monitors the system clipboard for changes and automatically updates the clipboard history.
+- Insert Text: Users can insert text from the clipboard history into any active text field with a single click.
+- Delete Text: Unwanted items in the clipboard history can be removed with the delete function.
+- Minimalist GUI: The user interface is clean and intuitive, providing easy access to clipboard history.
+
+## Quick start
+1. Clone the repository:
+```
+$ git clone https://github.com/anekobtw/clipboard-manager.git
+```
+
+2. Install dependencies:
+```
+$ pip install -r requirements.txt
+```
+
+3. Run the application
+```
+$ python clipboard.py
+```
+
+4. Press Ctrl + C to capture text copied to the clipboard.
+5. Press Win + V to toggle the visibility of the Clipboard Manager window.
+6. Use the "Insert" button to insert selected text from the clipboard history.
+7. Use the "Delete" button to remove selected text from the clipboard history.
+
+## Acknowledgements
+- [customtkinter](https://github.com/TomSchimansky/CustomTkinter) - A modern and customizable python UI-library based on Tkinter 
+- [keyboard](https://github.com/boppreh/keyboard) - Hook and simulate global keyboard events on Windows and Linux.
+- [pyperclip](https://github.com/asweigart/pyperclip) - Python module for cross-platform clipboard functions.
 
 ## Contributing
+Contributions are always welcome! If you have any suggestions, feature requests, or bug reports, please feel free to open an issue on the [GitHub repository](https://github.com/anekobtw/clipboard-manager).
 
-Contributions are always welcome! If you have any suggestions, feature requests, or bug reports, please feel free to open an issue on the GitHub repository.
+## Licence
+Copyright © 2024 anekobtw.\
+This project is [MIT](https://github.com/anekobtw/clipboard-manager/blob/main/LICENSE) licensed.
--- clipboard.py
+++ clipboard.py
@@ -1,34 +1,54 @@
 import threading
 import time
 from tkinter import Tk
-from typing import NoReturn
 
 import customtkinter
 import keyboard
 import pyperclip
 
+VERSION = "1.0.1"
+
 
 class ClipboardManagerApp(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.title('Clipboard Manager')
+        self.title(f"Clipboard Manager {VERSION}")
         self.clipboard = []
         self.create_widgets()
 
     def create_widgets(self) -> None:
-        self.option_menu = customtkinter.CTkOptionMenu(self, values=self.clipboard)
+        self.option_menu = customtkinter.CTkOptionMenu(
+            self,
+            variable=customtkinter.StringVar(self, value="Clipboard is empty"),
+            values=self.clipboard,
+            state="disabled",
+        )
         self.option_menu.pack(padx=5, pady=5)
 
-        self.insert_button = customtkinter.CTkButton(self, text='Insert', command=self.insert_text)
+        self.insert_button = customtkinter.CTkButton(self, text="Insert", fg_color="green", command=self.insert_text)
+        self.insert_button.pack(padx=5, pady=5)
+
+        self.insert_button = customtkinter.CTkButton(self, text="Delete", fg_color="red", command=self.delete_text)
         self.insert_button.pack(padx=5, pady=5)
 
     def update_option_menu(self) -> None:
-        self.option_menu.configure(values=self.clipboard)
+        if self.clipboard:
+            self.option_menu.configure(variable=customtkinter.StringVar(self, value="Choose an option"), values=self.clipboard)
+        else:
+            self.option_menu.configure(variable=customtkinter.StringVar(self, value="Clipboard is empty"), values=self.clipboard)
+        self.option_menu.configure(state="enabled") if self.clipboard else self.option_menu.configure(state="disabled")
 
     def insert_text(self) -> None:
-        text = self.option_menu.get()
+        if self.option_menu._state == "enabled":
+            self.withdraw()
+            keyboard.write(self.option_menu.get())
+        else:
+            self.withdraw()
+
+    def delete_text(self) -> None:
+        self.clipboard.remove(self.option_menu.get())
+        self.update_option_menu()
         self.withdraw()
-        keyboard.write(text)
 
 
 def get_clipboard_data(app: ClipboardManagerApp) -> None:
@@ -43,24 +63,24 @@ def get_clipboard_data(app: ClipboardManagerApp) -> None:
         print(f"Error getting clipboard data: {e}")
 
 
-def keyboard_listener1(app: ClipboardManagerApp) -> NoReturn:
+def keyboard_listener1(app: ClipboardManagerApp) -> None:
     while True:
-        keyboard.wait('ctrl + c')
+        keyboard.wait("ctrl + c")
         get_clipboard_data(app)
 
 
-def keyboard_listener2(app: ClipboardManagerApp) -> NoReturn:
+def keyboard_listener2(app: ClipboardManagerApp) -> None:
     while True:
-        keyboard.wait('win + v')
+        keyboard.wait("win + v")
         if app.state() == "withdrawn":
             x, y = Tk().winfo_pointerxy()
             app.deiconify()
-            app.geometry(f'+{x}+{y}')
+            app.geometry(f"+{x}+{y}")
         else:
             app.withdraw()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = ClipboardManagerApp()
 
     keyboard_thread1 = threading.Thread(target=keyboard_listener1, args=(app,), daemon=True)
